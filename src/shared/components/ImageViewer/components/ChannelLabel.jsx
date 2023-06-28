067856c1ee833034f79629b23b0094e7cfcdda8f
Aleksandr Prilipko <zverozabr@yandex.ru>
2023-06-28 09:08:18 +0700

    fixed channel font workbrek on sidebar

--- src/shared/components/ImageViewer/components/ChannelLabel.jsx
+++ src/shared/components/ImageViewer/components/ChannelLabel.jsx
@@ -5,11 +5,11 @@ export default styled.div`
   flex-wrap: nowrap;
   align-items: center;
   margin: 0 15px;
-  
+  font-size: 0.8em;
+  word-break: break-word;
+
   min-width: 50px;
   max-width: 50px;
 
-  white-space: nowrap;
-  text-overflow: ellipsis;
   overflow-x: hidden;
 `;
