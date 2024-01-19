c769400b36e71daca159e5d8f7a803c5349682b3
Aleksandr Prilipko <zverozabr@yandex.ru>
2024-01-19 16:37:45 +0700

    changed build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -f ./frontend/Dockerfile -t spex.frontend .
+docker build -f ./frontend/Dockerfile -t spex.frontend:latest .
+docker tag spex.frontend:latest ghcr.io/genentech/spex.frontend:latest
+docker push ghcr.io/genentech/spex.frontend:latest
