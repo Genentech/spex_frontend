b347510869f85f86106fd97d654547ee28ab2cba
Aleksandr Prilipko <zverozabr@yandex.ru>
2024-02-16 16:04:01 +0700

    deleted build

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -f ./frontend/Dockerfile -t spex.frontend:latest .
-docker tag spex.frontend:latest ghcr.io/genentech/spex.frontend:latest
-docker push ghcr.io/genentech/spex.frontend:latest
