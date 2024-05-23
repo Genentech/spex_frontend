e34d34bac416f4092f44b2458995dd714b7db711
Aleksandr Prilipko <31770101+zverozabr@users.noreply.github.com>
2024-05-23 12:44:38 +0700

    Revert "Added TIFF format for loading, fixed bugs"

--- src/shared/components/SelectFile/index.jsx
+++ src/shared/components/SelectFile/index.jsx
@@ -1,5 +1,4 @@
-import React, { useCallback, useState, useEffect, useRef } from 'react';
-import { actions as fileActions, selectors as fileSelectors } from '@/redux/modules/files';
+import React, { useCallback, useState } from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import TextField from '@material-ui/core/TextField';
@@ -10,132 +9,119 @@ import { actions as fileActions } from '@/redux/modules/files';
 import Button, { ButtonSizes, ButtonColors } from '+components/Button';
 
 const FilePicker = ({ input, meta, onFileChange, ...tail }) => {
-    const dispatch = useDispatch();
-    const ref = useRef(null);
-    const [selectedFile, setSelectedFile] = useState(null);
-    const [reset, setReset] = useState(false);
+  const dispatch = useDispatch();
+  const [selectedFile, setSelectedFile] = useState(null);
 
-    const isFetching = useSelector(fileSelectors.isFetching);
+  const showError =
+    ((meta.submitError && !meta.dirtySinceLastSubmit) || meta.error) && meta.touched;
 
-    useEffect(() => {
-        if (!isFetching && selectedFile && reset) {
-            setSelectedFile(null);
-            setReset(false);
+  const onChange = input.onChange || tail.onChange;
+
+  const handleFileChange = useCallback(
+    (e) => {
+      const file = e.target.files[0];
+      setSelectedFile(file);
+    },
+    [],
+  );
+
+  const handleUpload = useCallback(async () => {
+    try {
+      if (selectedFile) {
+        onChange?.(selectedFile);
+        if (typeof onFileChange === 'function') {
+          onFileChange(selectedFile);
         }
-    },[isFetching, selectedFile, reset]);
+        dispatch(fileActions.uploadFile(selectedFile));
+      }
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+    }
+  }, [dispatch, selectedFile, onChange, onFileChange]);
 
-    const showError =
-        ((meta.submitError && !meta.dirtySinceLastSubmit) || meta.error) && meta.touched;
-
-    const onChange = input.onChange || tail.onChange;
-
-    const handleFileChange =
-        (e) => {
-            const file = e.target.files[0];
-            setSelectedFile(file);
-        };
-
-    const handleUpload = useCallback(async () => {
-        try {
-            if (selectedFile) {
-                onChange?.(selectedFile);
-                if (typeof onFileChange === 'function') {
-                    onFileChange(selectedFile);
-                }
-                dispatch(fileActions.uploadFile(selectedFile));
-                setReset(true);
-            }
-        } catch (error) {
-            // eslint-disable-next-line no-console
-            console.error(error);
-        }
-    }, [dispatch, selectedFile, onChange, onFileChange]);
-
-    const openFileDialog = () => {
-        ref.current.value = '';
-        ref.current.click();
-    };
-
-    const renderInput = useCallback(() => {
-        return (
-          <React.Fragment>
-            <input
-              {...tail}
-              type="file"
-              ref={ref}
-              onChange={handleFileChange}
-              accept=".h5ad, .tiff, *"
-              style={{ display: 'none' }}
-            />
-
-
-            <TextField
-              InputProps={{
-                        readOnly: true,
-                        value: selectedFile ? selectedFile.name : '',
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <IconButton
-                              edge="end"
-                              aria-label="select file"
-                              component="span"
-                              onClick={openFileDialog}
-                              disabled={!!isFetching}
-                            >
-                              <FolderOpenIcon />
-                            </IconButton>
-                          </InputAdornment>
-                        ),
-              }}
-              disabled={!!isFetching}
-              helperText={showError ? meta.error || meta.submitError : undefined}
-              error={showError}
-              label={tail.label || 'Select'}
-              variant="outlined"
-              style={{ maxWidth: '50%', flexGrow: 0, transform: 'scaleY(0.80)' }}
-            />
-          </React.Fragment>
-        );
-    }, [tail, ref, selectedFile, openFileDialog, isFetching, showError, meta.error, meta.submitError]);
-
+  const renderInput = useCallback(() => {
     return (
-      <div>
-        <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
-          {renderInput()}
+      <React.Fragment>
+        <input
+          {...tail}
+          type="file"
+          id="file-picker-input"
+          onChange={handleFileChange}
+          accept=".h5ad, *"
+          style={{ display: 'none' }}
+        />
 
-          <Button
-            size={ButtonSizes.medium}
-            color={ButtonColors.primary}
-            onClick={handleUpload}
-            disabled={isFetching || !selectedFile}
-          >
-            Upload
-          </Button>
-        </div>
-      </div>
+        <label
+          htmlFor="file-picker-input"
+          style={{ position: 'relative', cursor: 'pointer' }}
+          onClick={(event) => {
+            if (!selectedFile) {
+              handleUpload();
+              document.getElementById('file-picker-input').click();
+            }
+          }}
+        >
+          <TextField
+            InputProps={{
+              readOnly: true,
+              value: selectedFile ? selectedFile.name : '',
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton edge="end" aria-label="select file" component="span">
+                    <FolderOpenIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+            helperText={showError ? meta.error || meta.submitError : undefined}
+            error={showError}
+            label={tail.label || 'Select'}
+            variant="outlined"
+            style={{ flexGrow: 1, transform: 'scaleY(0.80)' }}
+          />
+        </label>
+      </React.Fragment>
     );
+  }, [tail, showError, meta.error, meta.submitError, selectedFile, handleFileChange, handleUpload]);
+
+  return (
+    <div>
+      <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
+        {renderInput()}
+
+        <Button
+          size={ButtonSizes.medium}
+          color={ButtonColors.primary}
+          onClick={handleUpload}
+          disabled={!selectedFile}
+        >
+          Upload
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 FilePicker.propTypes = {
-    input: PropTypes.shape({
-        onChange: PropTypes.func,
-    }),
-    meta: PropTypes.shape({
-        error: PropTypes.string,
-        touched: PropTypes.bool,
-        submitError: PropTypes.string,
-        dirtySinceLastSubmit: PropTypes.bool,
-    }),
+  input: PropTypes.shape({
     onChange: PropTypes.func,
-    onFileChange: PropTypes.func,
+  }),
+  meta: PropTypes.shape({
+    error: PropTypes.string,
+    touched: PropTypes.bool,
+    submitError: PropTypes.string,
+    dirtySinceLastSubmit: PropTypes.bool,
+  }),
+  onChange: PropTypes.func,
+  onFileChange: PropTypes.func,
 };
 
 FilePicker.defaultProps = {
-    input: {},
-    meta: {},
-    onChange: null,
-    onFileChange: null,
+  input: {},
+  meta: {},
+  onChange: null,
+  onFileChange: null,
 };
 
 export default FilePicker;
-
